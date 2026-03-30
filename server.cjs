361514108872753b116dd897f22b64d0c127f9c2
Ahmed Zaki <ahmedzaki695@gmail.com>
2026-03-30 19:27:19 +0200

    update backend

---
+++ server.cjs
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
